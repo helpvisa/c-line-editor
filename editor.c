ee00265099de9d3fb4690f092ec1b3c757057b72
Daniel Brackenbury <daniel.brackenbury@gmail.com>
2024-01-11 03:54:04 -0500

    added file loading and line deletion

--- editor.c
+++ editor.c
@@ -16,8 +16,10 @@ void strip_newline(char s[]); // remove newline characters from a given input st
 int squeeze(char s[], char c); // remove given character from a string
 int line_squeeze(char s1[], char s2[]); // remove from s1 all characters occurring in s2
 void condense(char s[]); // removes any recurring instance of any character in the given string
+void shift_lines_down(int from, int to); // shifts all lines down starting at a point and ending at another point
 // file functions
 void save_file(FILE *fptr);
+void read_file(FILE *fptr);
 // misc functions
 int count_chars(char s[]);
 void print_lines();
@@ -38,7 +40,7 @@ enum IsActive { // whether the application should keep the core input loop alive
 
 // global variable definitions
 FILE *f; // pointer to our file
-char *path = "test_output.txt"; // path to our file (temporarily set here for testing)
+char *path; // path to our file (temporarily set here for testing)
 char *prompt = ">"; // the command prompt
 char command = '.'; // the character which prompts command parsing
 int mode = PROMPT;
@@ -48,7 +50,18 @@ char order[LIMIT]; // the last order input at the prompt
 int line_idx = 1; // line we are currently on; we use 1 so we can use '0' to prepend the first line of the file
 int total_lines = 0; // the total number of lines with data on them
 
-int main() {
+int main(int argc, char *argv[]) {
+  // process args
+  printf("Welcome to %s!\nType 'h' for a list of commands.\n", argv[0]);
+  if (argc > 1) {
+    path = argv[1];
+    read_file(f);
+    printf("Editing %s.\n", path);
+  } else {
+    path = "untitled.txt";
+    printf("No file specified. Editing 'untitled.txt'.\nUse 'f' to set filename.\n");
+  }
+
   while (active == TRUE) {
     switch (mode) {
       case INSERT:
@@ -100,14 +113,25 @@ int read_input(char s[]) {
   } else if (mode == PROMPT) {
     // check for a blank command prompt
     if (s[0] == '\0') {
-      printf("?BLANK COMMAND?\n");
+      printf("? Blank command.\n");
     } else if (s[0] != command) {
+      int stop_parsing = 0; // if 1, do not execute next command
       // check following characters
-      for (int i = 0; s[i] != '\0'; i++) {
+      for (int i = 0; s[i] != '\0' && stop_parsing == 0; i++) {
         switch (s[i]) {
           case 'i':
             mode = INSERT;
             printf("Entering insert mode...\n");
+            break;
+          case 'd':
+            lines[line_idx][0] = '\0';
+              if (line_idx <= total_lines) {
+              shift_lines_down(line_idx,total_lines);
+              total_lines--; // we have deleted one line
+              printf("Current line (%d) deleted.\n", line_idx);
+              if (line_idx > total_lines+1)
+                line_idx = total_lines+1;
+            } else printf("Current line (%d) is already empty.\n", line_idx);
             break;
           case 'q':
             active = FALSE;
@@ -124,9 +148,23 @@ int read_input(char s[]) {
           case 'c':;
             int count = count_chars(lines[line_idx]);
             printf("%d character%s on current line (%d).\n", count, (count > 1 || count < 1) ? "s" : "", line_idx);
+            printf("%s\n", lines[line_idx]);
             break;
           case 'l':
             printf("%d total lines in document.\n", total_lines);
+            break;
+          case 'f':;
+            char new_name[LIMIT];
+            int length = 0;
+            for (int j = i+1; s[j] != '\0' && s[j] != '\n'; j++) {
+              new_name[j-i-1] = s[j];
+              length++;
+            }
+            if (length > 0) {
+              path = new_name;
+              printf("Changed filename to %s\n", path);
+            } else printf("No filename entered!\nEditing %s\n", path);
+            stop_parsing = 1;
             break;
           case 'g':;
             int goto_line = 0; // the line we target
@@ -143,7 +181,7 @@ int read_input(char s[]) {
             break;
           default:
             if (!(s[i] >= '0' && s[i] <= '9'))
-              printf("?COMMAND %d NOT RECOGNIZED?\n", i);
+              printf("? Command '%c' not recognized.\n", s[i]);
             break;
         }
       }
@@ -173,6 +211,13 @@ void strip_newline(char s[]) {
   s[j] = '\0';
 }
 
+// shift all lines down from the start point up to the end point
+void shift_lines_down(int from, int to) {
+  for (int i = from; i < to; i++) {
+    copy_string(lines[i+1], lines[i]);
+  }
+}
+
 // save the open file to disk
 void save_file(FILE *fptr) {
   int total_characters = 0; // total number of characters saved to disk
@@ -186,6 +231,18 @@ void save_file(FILE *fptr) {
   fclose(fptr); // close our open file (free mem, unlock it for other programs)
 
   printf("\033[33m%d bytes saved to disk at %s.\033[0m\n", total_characters, path);
+}
+
+// open and read the specified file
+void read_file(FILE *fptr) {
+  fptr = fopen(path, "r");
+  line_idx = 1; // we start reading from line 1
+  while (fgets(lines[line_idx], LIMIT, fptr)) {
+    strip_newline(lines[line_idx]);
+    total_lines = line_idx;
+    line_idx++;
+  }
+  fclose(fptr);
 }
 
 // print the document
